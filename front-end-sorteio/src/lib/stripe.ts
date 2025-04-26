3d9d5a852a6a5e05d77f2595034d18e4f46778b4
andreluisx <andreluissc1214@gmail.com>
2025-04-26 13:58:59 -0300

    arquivos para configuração do stripe adicionados (vazios)

--- front-end-sorteio/src/lib/stripe.ts
+++ front-end-sorteio/src/lib/stripe.ts
@@ -1,5 +1,13 @@
-import 'server-only'
+import { loadStripe } from '@stripe/stripe-js';
+import Stripe from 'stripe';
 
-import Stripe from 'stripe'
+// Cliente frontend
+export const getStripe = async () => {
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || '');
+  return stripePromise;
+};
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+// Cliente backend
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+  apiVersion: '2025-03-31.basil', // Use a versão mais recente
+});
